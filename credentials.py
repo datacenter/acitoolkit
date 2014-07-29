b5944afba34feedc967c2c19a1a736b478194fbc
Michael Smith <michsmit@cisco.com>
2014-07-28 20:23:20 -0700

    Adding files into Guthub

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Fill in with the APIC admin userid
+LOGIN = 'admin'
+# Fill in with the APIC admin password
+PASSWORD = 'password'
+# Fill in with the APIC IP address
+IPADDR = '1.2.3.4'
+URL = 'http://' + IPADDR + ':80/'
