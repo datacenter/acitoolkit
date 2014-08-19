5d3a8517838affcbb67f9f0b39a46c653d34a6f9
Michael Smith <michsmit@cisco.com>
2014-08-19 14:14:53 -0700

    Added sample file

---
+++ samples/aci-attach-epg-to-interface.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+Simple application to statically connect an EPG to a specific interface using
+a specific VLAN.
+
+It logs in to the APIC and will create the tenant, application profile,
+and EPG if they do not exist already.  It then connects it to the specified
+interface using the VLAN encapsulation specified.
+
+Before running, please make sure that the credentials.py
+file has the URL, LOGIN, and PASSWORD set for your APIC environment.
+"""
+import acitoolkit as ACI
+import credentials
+
+# Define static values to pass (edit these if you wish to set differently)
+TENANT_NAME = 'cisco'
+APP_NAME = 'fancyapp'
+EPG_NAME = 'redis'
+INTERFACE = {'type': 'eth',
+             'pod': '1', 'node': '101', 'module': '1', 'port': '65'}
+VLAN = {'name': 'vlan5',
+        'encap_type': 'vlan',
+        'encap_id': '5'}
+
+# Login to the APIC
+session = ACI.Session(credentials.URL, credentials.LOGIN, credentials.PASSWORD)
+resp = session.login()
+if not resp.ok:
+    print '%% Could not login to APIC'
+
+# Create the Tenant, App Profile, and EPG
+tenant = ACI.Tenant(TENANT_NAME)
+app = ACI.AppProfile(APP_NAME, tenant)
+epg = ACI.EPG(EPG_NAME, app)
+
+# Create the physical interface object
+intf = ACI.Interface(INTERFACE['type'],
+                     INTERFACE['pod'],
+                     INTERFACE['node'],
+                     INTERFACE['module'],
+                     INTERFACE['port'])
+
+# Create a VLAN interface and attach to the physical interface
+vlan_intf = ACI.L2Interface(VLAN['name'], VLAN['encap_type'], VLAN['encap_id'])
+vlan_intf.attach(intf)
+
+# Attach the EPG to the VLAN interface
+epg.attach(vlan_intf)
+
+# Push it all to the APIC
+resp = session.push_to_apic(tenant.get_url(),
+                            tenant.get_json())
+if not resp.ok:
+    print '%% Error: Could not push configuration to APIC'
