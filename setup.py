13340c9ebdcebdde63e6fde1c39ccb2ed06b9124
Michael Smith <michsmit@cisco.com>
2014-10-28 14:51:25 -0700

    Added email

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version = "0.1",
     packages = ["acitoolkit"],
     author = "Cisco Systems, Inc.",
+    author_email = "acitoolkit@cisco.com",
     url = "http://github.com/datacenter/acitoolkit/",
     license = "http://www.apache.org/licenses/LICENSE-2.0",
     install_requires = ["requests"],
