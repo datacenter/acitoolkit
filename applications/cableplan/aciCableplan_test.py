51d3b6339423a893663713c11f390fea2208b1cf
Tom Edsall <edsall@cisco.com>
2014-11-04 22:24:53 -0800

    added copyright

--- applications/cableplan/aciCableplan_test.py
+++ applications/cableplan/aciCableplan_test.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2014 Cisco Systems
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
 from acitoolkit.acisession import Session
 from credentials import *
 from acitoolkit.acitoolkit import *
@@ -6,7 +21,7 @@ import sys
 import unittest
 import cableplan
 import os
-LIVE_TEST = True
+LIVE_TEST = False
 
 class Test_ParseXML(unittest.TestCase) :
     def get_temporary_filename(self) :
