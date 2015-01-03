b862f2d3203d0870177966827c2125fb1b152e71
Tom Edsall <edsall@cisco.com>
2015-01-03 15:35:12 -0700

    Added several command line options

--- samples/aci-show-interface-stats.py
+++ samples/aci-show-interface-stats.py
@@ -26,6 +26,17 @@ from acisampleslib import get_login_info
 # Otherwise, take them from your environment variables file ~/.profile
 description = 'Simple application that logs on to the APIC and displays stats for all of the Interfaces.'
 parser = get_login_info(description)
+parser.add_argument('-i', '--interface',
+                    type=str, 
+                    help='Specify a particular interface node/pod/module/port e.g. 1/201/1/21')
+parser.add_argument('-e', '--epoch', type=int,
+                    default = 0,
+                    help='Show a particular epoch (default=0)')
+parser.add_argument('-g', '--granularity', type=str,
+                    default = '5min',
+                    help='Show a particular granularity (default="5min")')
+parser.add_argument('-f', '--full', action="store_true",
+                    help='Show full statistics - only available if interface is specified')
 args = parser.parse_args()
 
 # Login to APIC
@@ -35,27 +46,53 @@ if not resp.ok:
     print '%% Could not login to APIC'
     sys.exit(0)
 
-# setup template and display header information
-template = "{0:17} {1:12} {2:12} {3:16} {4:16} {5:16} {6:16}"
-print template.format("   INTERFACE  ", "TOT RX PACKETS", "TOT TX PACKETS", "RX PKTs/Sec", "TX PKTs/Sec", "RX BYTES/Sec", "TX BYTES/Sec")
-print template.format("--------------", "------------ ", "------------ ", "---------------", "---------------", "---------------", "---------------")
-template = "{0:17} {1:12,} {2:12,} {3:16,.2f} {4:16,.2f} {5:16,.2f} {6:16,.2f}"
+def show_stats_short() :
     
+    # setup template and display header information
+    template = "{0:17} {1:12} {2:12} {3:16} {4:16} {5:16} {6:16}"
+    print 'Granularity:',args.granularity, 'Epoch:',args.epoch
+    print template.format("   INTERFACE  ", "TOT RX PACKETS", "TOT TX PACKETS", "RX PKTs/Sec", "TX PKTs/Sec", "RX BYTES/Sec", "TX BYTES/Sec")
+    print template.format("--------------", "------------ ", "------------ ", "---------------",
+                          "---------------", "---------------", "---------------")
+    template = "{0:17} {1:12,} {2:12,} {3:16,.2f} {4:16,.2f} {5:16,.2f} {6:16,.2f}"
+    
+    for interface in sorted(interfaces):
+        interface.stats.get()
+        
+        rec = []
+        for (counterFamily, counterName) in [('ingrTotal','pktsAvg'),('egrTotal','pktsAvg'),
+                                ('ingrTotal','pktsRateAvg'),('egrTotal','pktsRateAvg'),
+                                ('ingrTotal','bytesRateAvg'),('egrTotal','bytesRateAvg')] :
+            
+            rec.append(interface.stats.retrieve(counterFamily,args.granularity,args.epoch,counterName))
+            
+        print template.format(interface.name, *rec)
+        
+def show_stats_long() :
+    print 'Interface {0}/{1}/{2}/{3}  Granularity:{4} Epoch:{5}'.format(pod,node,module,port,args.granularity, args.epoch)
+    stats = interfaces[0].stats.get()
+    for statsFamily in sorted(stats) :
+        print statsFamily
+        if args.granularity in stats[statsFamily] :
+            if args.epoch in stats[statsFamily][args.granularity] :
+                for counter in sorted(stats[statsFamily][args.granularity][args.epoch]) :
+                    print '    {0:>16}: {1}'.format(counter, stats[statsFamily][args.granularity][args.epoch][counter])
+        
+
 # Download all of the interfaces and get their stats
 # and display the stats
-data = []
-interfaces = ACI.Interface.get(session)
-for interface in sorted(interfaces):
-    interface.stats.get()
+
+if args.interface :
+    (pod, node, module, port) = args.interface.split('/')
+    interfaces = ACI.Interface.get(session, pod, node, module, port)
+else :
+    interfaces = ACI.Interface.get(session)
+
+if not args.full or not args.interface :
+    show_stats_short()
+else :
+    show_stats_long()
     
-    rec = []
-    for (counterFamily, counterName) in [('ingrTotal','pktsAvg'),('egrTotal','pktsAvg'),
-                            ('ingrTotal','pktsRateAvg'),('egrTotal','pktsRateAvg'),
-                            ('ingrTotal','bytesRateAvg'),('egrTotal','bytesRateAvg')] :
-        
-        rec.append(interface.stats.retrieve(counterFamily,'5min',0,counterName))
-        
-    print template.format(interface.name, *rec)
 
 
     
