63ef2803be323fc5830b821a949fbbd92c0a4aff
Luis MartinGarcia <luis.mgarc@gmail.com>
2015-04-06 10:43:14 +0200

    ACI Lint App: Update copyright statement to year 2015 and add a better looking ASCII header.

--- applications/lint/acilint.py
+++ applications/lint/acilint.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
-# Copyright (c) 2015 Cisco Systems
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-#
+################################################################################
+#                        _    ____ ___   _     _       _                       #
+#                       / \  / ___|_ _| | |   (_)_ __ | |_                     #
+#                      / _ \| |    | |  | |   | | '_ \| __|                    #
+#                     / ___ \ |___ | |  | |___| | | | | |_                     #
+#                    /_/   \_\____|___| |_____|_|_| |_|\__|                    #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Copyright (c) 2015 Cisco Systems                                             #
+# All Rights Reserved.                                                         #
+#                                                                              #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may   #
+#    not use this file except in compliance with the License. You may obtain   #
+#    a copy of the License at                                                  #
+#                                                                              #
+#         http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                              #
+#    Unless required by applicable law or agreed to in writing, software       #
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT #
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  #
+#    License for the specific language governing permissions and limitations   #
+#    under the License.                                                        #
+#                                                                              #
+################################################################################
 """
 acilint - A static configuration analysis tool for examining ACI Fabric
           configuration for potential problems and unused configuration.
