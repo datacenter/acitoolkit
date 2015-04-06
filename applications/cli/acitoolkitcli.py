c3b12917966c2d1195b465b3fb21624714d26708
Luis MartinGarcia <luis.mgarc@gmail.com>
2015-04-06 10:33:30 +0200

    AciToolkitCLI Application: Add copyright statement (didn't have any) inside a proper ASCII header.

--- applications/cli/acitoolkitcli.py
+++ applications/cli/acitoolkitcli.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+################################################################################
+#           _    ____ ___ _____           _ _    _ _    ____ _     ___         #
+#          / \  / ___|_ _|_   _|__   ___ | | | _(_) |_ / ___| |   |_ _|        #
+#         / _ \| |    | |  | |/ _ \ / _ \| | |/ / | __| |   | |    | |         #
+#        / ___ \ |___ | |  | | (_) | (_) | |   <| | |_| |___| |___ | |         #
+#       /_/   \_\____|___| |_|\___/ \___/|_|_|\_\_|\__|\____|_____|___|        #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Copyright (c) 2015 Cisco Systems                                             #
+# All Rights Reserved.                                                         #
+#                                                                              #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may   #
+#    not use this file except in compliance with the License. You may obtain   #
+#    a copy of the License at                                                  #
+#                                                                              #
+#         http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                              #
+#    Unless required by applicable law or agreed to in writing, software       #
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT #
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  #
+#    License for the specific language governing permissions and limitations   #
+#    under the License.                                                        #
+#                                                                              #
+################################################################################
 
 """This module implements a CLI similar to Cisco IOS and NXOS
    for use with the ACI Toolkit.
