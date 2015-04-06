b0cd7edd3a437bc22a72325c7a0a1cf9b353815d
Luis MartinGarcia <luis.mgarc@gmail.com>
2015-04-06 10:46:34 +0200

    ACI Multi-Site App: Add a copyright statement to most files.

--- applications/multiSite/copy_application.py
+++ applications/multiSite/copy_application.py
@@ -1,3 +1,28 @@
+################################################################################
+#            _    ____ ___   __  __       _ _   _      ____  _ _               #
+#           / \  / ___|_ _| |  \/  |_   _| | |_(_)    / ___|(_) |_ ___         #
+#          / _ \| |    | |  | |\/| | | | | | __| |____\___ \| | __/ _ \        #
+#         / ___ \ |___ | |  | |  | | |_| | | |_| |_____|__) | | ||  __/        #
+#        /_/   \_\____|___| |_|  |_|\__,_|_|\__|_|    |____/|_|\__\___|        #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Copyright (c) 2015 Cisco Systems                                             #
+# All Rights Reserved.                                                         #
+#                                                                              #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may   #
+#    not use this file except in compliance with the License. You may obtain   #
+#    a copy of the License at                                                  #
+#                                                                              #
+#         http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                              #
+#    Unless required by applicable law or agreed to in writing, software       #
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT #
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  #
+#    License for the specific language governing permissions and limitations   #
+#    under the License.                                                        #
+#                                                                              #
+################################################################################
 import ast
 import sys
 import push_or_pull_github
