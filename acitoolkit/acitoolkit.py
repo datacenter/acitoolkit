39401c7b09255f4a9cf888c537e4f9b541dea728
Kevin Corbin <kecorbin@cisco.com>
2015-07-20 12:30:27 -0500

    Standardize unicast routing attribute as BridgeDomain.unicast_route.

--- acitoolkit/acitoolkit.py
+++ acitoolkit/acitoolkit.py
@@ -1813,7 +1813,7 @@ class BridgeDomain(BaseACIObject):
         self.vnid = attributes.get('seg')
         self.mtu = attributes.get('mtu')
         self.mac = attributes.get('mac')
-        self.route = attributes.get('unicastRoute')
+        self.unicast_route = attributes.get('unicastRoute')
         self.unknown_unicast = attributes.get('unkMacUcastAct')
         self.unknown_multicast = attributes.get('unkMcastAct')
         self.modified_time = attributes.get('modTs')
@@ -1856,7 +1856,7 @@ class BridgeDomain(BaseACIObject):
                 bridge_domain.name,
                 ', '.join(subnet_str),
                 bridge_domain.mac,
-                bridge_domain.route,
+                bridge_domain.unicast_route,
                 bridge_domain.unknown_unicast,
                 bridge_domain.unknown_multicast,
                 bridge_domain.vnid,
