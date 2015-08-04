1ac0f76cf9ba22c23f09ccc0c67076667f4b92d2
Michael Smith <michsmit@cisco.com>
2015-08-04 14:17:18 -0700

    Increment version number

--- acitoolkit/__about__.py
+++ acitoolkit/__about__.py
@@ -19,7 +19,7 @@ __title__ = "acitoolkit"
 __summary__ = "Toolkit for Cisco ACI Fabrics"
 __uri__ = "http://datacenter.github.io/acitoolkit/"
 
-__version__ = "0.3"
+__version__ = "0.3.1"
 
 __author__ = "Cisco Systems, Inc."
 __email__ = "acitoolkit@external.cisco.com"
