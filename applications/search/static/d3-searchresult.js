85ea0e578e99d8ff6f5603bed05d1dfa25a3b71d
Tom Edsall <edsall@cisco.com>
2015-10-13 13:25:25 -0700

    updated

--- applications/search/static/d3-searchresult.js
+++ applications/search/static/d3-searchresult.js
@@ -1,34 +1,24 @@
-/*
-
- <br>
- <hr>
- <h4>Name:{{ search_item['name']|safe }}</h4>
- <h4>Object:{{ search_item.object_type|safe }}</h4>
- <h4>Primary score:{{ search_item.pscore|safe }}</h4>
- <h4>Secondary score:{{ search_item.sscore|safe }}</h4>
- <h4>path{{ search_item.path|safe }}</h4>
- <h4>matching terms{{ search_item.terms|safe }}</h4>
- <br>
- <hr>
- {%  for table in search_item.report_table %}
- <script>
- </script>
- {%  endfor %}
- */
-
-function searchResult(report) {
-
+function onClick_show_object(d) {
+    var uri = '/atk_object?dn='+d['path']
+    window.location.assign(encodeURI(uri))
+}
+function searchResult(result) {
+    var report = result['result'];
+    var total_hits = result['total_hits'];
     function gen_summary(d) {
         var strng = "";
         strng += '<hr>';
-        strng += "<h4>Name:"+d.name+ "</h4>";
-        strng += "<p>Type:"+d.object_type+ "<br>";
-        strng += 'Match score:' + d.pscore + '.' + d.sscore + '<br>';
+        strng += "<h4>"+d.name+ "</h4>";
         strng += 'Path:' + d.path + '<br>';
+        strng += "<p>Class:"+d.object_type+ ", ";
+        strng += 'Match score:' + d.pscore + '.' + d.sscore + ', ';
         strng += 'Matching terms:' + d.terms + '</p>';
         return strng;
     }
     // determine where to place it and create place holder
+    d3.select(".span10").select("#records")
+        .remove();
+
     d3.select(".span10").select("#content")
         .data(report)
         .append("div")
@@ -49,40 +39,16 @@ function searchResult(report) {
         .attr("class", "record")
         .append("div")
         .attr("class", "record_summary")
-        .html(function (d) {return gen_summary(d);});
-
-    d3.select("#records")
-        .selectAll(".record")
-        .append("div")
-        .attr("class", "table_summary")
-
-    for (var index in report) {
-        var search_item = report[index];
-
-        for (var table_index in search_item['report_table']) {
-            var table = search_item['report_table'][table_index];
-            var cleanKey = function (d) {
-                return d.replace(/\//g, "_").replace(/\W/g, "_");
-            };
-            var table_data = table.data;
-            var table_columns = table.headers;
-            //
-            var table_title = table.title_flask;
-
-            var dimensions = {width: '600px', height: '400px'};
-            var table_id = cleanKey('report_table' + table_title);
+        .html(function (d) {return gen_summary(d);})
+        .on("click", function (d, i) { onClick_show_object(d); });
 
 
-            if (table_data.length > 0) {
-                TableSort(
-                    table_title,
-                    index,
-                    table_columns,
-                    table_data,
-                    dimensions
-                );
-            }
-        }
+    if (report.length==0) {
+        d3.select('.ac-searching').text('No results').style("display", "block");
+    } else {
+        index = report.length;
+        d3.select('.ac-searching').text('Showing '+index+' of ' + total_hits+' results').style("display", "block");
+
     }
 
 }
