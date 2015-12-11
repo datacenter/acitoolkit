d2d885d689f8d8f9fbd828f2795d4be1b8c52f95
Michael Smith <michsmit@cisco.com>
2015-12-11 15:23:16 -0800

    Minor pep8/pylint cleanup

--- applications/visualizations/epg-bubbles.py
+++ applications/visualizations/epg-bubbles.py
@@ -1,3 +1,11 @@
+"""
+Visualization of Endpoint membership within an EPG.
+Runs as a web service and connects to the APIC to get Endpoint data.
+Bubbles represent the EPGs.
+Colorization is done based on Tenant/App/EPG and bubble size represents
+the number of Endpoints in the EPG.  Hovering over a bubble will show the
+EPG name and number of Endpoints.
+"""
 import flask
 import json
 import sys
@@ -6,9 +14,18 @@ from acitoolkit import Credentials, Session, Endpoint
 DEFAULT_PORT = '5000'
 DEFAULT_IPADDRESS = '127.0.0.1'
 
-app = flask.Flask(__name__)
+flask_app = flask.Flask(__name__)
+
 
 def get_data_from_apic(url, username, password):
+    """
+    Gets the Endpoint data from the APIC
+
+    :param url: String containing the URL of the APIC
+    :param username: String containing the username to login to the APIC
+    :param password: String containing the password to login to the APIC
+    :return: None
+    """
     ep_db = {}
 
     # Login to the APIC
@@ -67,19 +84,20 @@ def get_data_from_apic(url, username, password):
         sys.exit(0)
 
 
-@app.route('/')
+@flask_app.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
     return flask.render_template('bubble-tooltips.html')
 
 if __name__ == '__main__':
-    description = 'Endpoints per EPG bubble chart visualization'
-    creds = Credentials(('apic', 'server'), description)
+    creds = Credentials(('apic', 'server'),
+                        'Endpoints per EPG bubble chart visualization')
     args = creds.get()
 
     print 'Getting data from APIC....'
     get_data_from_apic(args.url, args.login, args.password)
 
-    print 'Running server. Point your browser to http://%s:%s'% (args.ip, args.port)
-    app.run(debug=False, host=args.ip, port=int(args.port))
+    print 'Running server. Point your browser to http://%s:%s' % (args.ip,
+                                                                  args.port)
+    flask_app.run(debug=False, host=args.ip, port=int(args.port))
