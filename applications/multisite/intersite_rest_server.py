32f959e560a20d1e0a42a8b0fc20423995ea4b6e
Michael Smith <michsmit@cisco.com>
2016-03-16 11:11:39 -0700

    Added docstrings

--- applications/multisite/intersite_rest_server.py
+++ applications/multisite/intersite_rest_server.py
@@ -1,3 +1,6 @@
+"""
+REST API service wrapper for the Intersite tool
+"""
 from flask import Flask, request, abort, make_response, jsonify
 from intersite import execute_tool, get_arg_parser
 import json
@@ -23,6 +26,11 @@ app = Flask(__name__)
 
 @auth.get_password
 def get_password(username):
+    """
+    Get the password
+    :param username: String containing the username
+    :return: String containing the password for the user or None if the user does not exist
+    """
     if username == 'admin':
         return 'acitoolkit'
     return None
@@ -30,18 +38,30 @@ def get_password(username):
 
 @auth.error_handler
 def unauthorized():
+    """
+    Error handler for attempted unauthorized access
+    :return: 401 Error Response
+    """
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 @app.route('/config', methods=['GET'])
 @auth.login_required
 def get_config():
+    """
+    Get the current configuration
+    :return: JSON dictionary containing the current configuration
+    """
     return json.dumps(collector.config.get_config(), indent=4, separators=(',', ':'))
 
 
 @app.route('/config', methods=['POST', 'PUT'])
 @auth.login_required
 def set_config():
+    """
+    Set the current configuration
+    :return: JSON dictionary with a Status of OK if successful
+    """
     if not request.json:
         abort(400)
     resp = collector.save_config(request.json)
