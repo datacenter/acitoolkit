13328a10654044a575585a648c4511b849e6eabf
Michael Smith <michsmit@cisco.com>
2016-03-21 09:16:46 -0700

    pylint cleanup

--- acitoolkit/__init__.py
+++ acitoolkit/__init__.py
@@ -9,8 +9,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .__about__ import ( # noqa
+"""
+__init__.py
+"""
+from .__about__ import (  # noqa
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__,
 )
@@ -20,33 +22,35 @@ _about_exports = [
     "__title__", "__uri__", "__version__",
 ]
 
-from .acibaseobject import Tag # noqa
-from .acicounters import ( # noqa
+from .acibaseobject import Tag  # noqa
+from .acicounters import (  # noqa
     AtomicCounter, AtomicCountersOnGoing, AtomicNode, AtomicPath,
     InterfaceStats,
 )
-from .aciHealthScore import HealthScore # noqa
-from .aciSearch import AciSearch, Searchable # noqa
-from .acisession import EventHandler, Login, Session, Subscriber # noqa
-from .aciTable import Table # noqa
-from .acitoolkit import ( # noqa
-    AppProfile, AttributeCriterion, BGPSession, BridgeDomain, CollectionPolicy, CommonEPG, Context,
-    Contract, ContractInterface, ContractSubject, Endpoint, EPG, EPGDomain, FexInterface,
-    Filter, FilterEntry, IPEndpoint, L2ExtDomain, L2Interface, L3ExtDomain, L3Interface,
-    LogicalModel, MonitorPolicy, MonitorStats, MonitorTarget, NetworkPool,
-    OSPFInterface, OSPFInterfacePolicy, OSPFRouter, OutsideEPG, OutsideL3, OutsideNetwork,
+from .aciHealthScore import HealthScore  # noqa
+from .aciSearch import AciSearch, Searchable  # noqa
+from .acisession import EventHandler, Login, Session, Subscriber  # noqa
+from .aciTable import Table  # noqa
+from .acitoolkit import (  # noqa
+    AppProfile, AttributeCriterion, BGPSession, BridgeDomain, CollectionPolicy,
+    CommonEPG, Context, Contract, ContractInterface, ContractSubject, Endpoint,
+    EPG, EPGDomain, FexInterface, Filter, FilterEntry, IPEndpoint, L2ExtDomain,
+    L2Interface, L3ExtDomain, L3Interface, LogicalModel, MonitorPolicy,
+    MonitorStats, MonitorTarget, NetworkPool, OSPFInterface,
+    OSPFInterfacePolicy, OSPFRouter, OutsideEPG, OutsideL3, OutsideNetwork,
     PhysDomain, PortChannel, Search, Subnet, Taboo, Tenant, TunnelInterface,
     VMM, VMMCredentials, VmmDomain, VMMvSwitchInfo,
 )
-from .acitoolkitlib import Credentials # noqa
+from .acitoolkitlib import Credentials  # noqa
 # Dependent on acitoolkit
-from .aciConcreteLib import ( # noqa
+from .aciConcreteLib import (  # noqa
     ConcreteAccCtrlRule, ConcreteArp, ConcreteBD, ConcreteContext, ConcreteEp,
     ConcreteFilter, ConcreteFilterEntry, ConcreteLoopback, ConcreteOverlay,
-    ConcretePortChannel, ConcreteSVI, ConcreteVpc, ConcreteVpcIf, ConcreteTunnel,
+    ConcretePortChannel, ConcreteSVI, ConcreteVpc, ConcreteVpcIf,
+    ConcreteTunnel,
 )
 # Dependent on aciconcretelib
-from .aciphysobject import ( # noqa
+from .aciphysobject import (  # noqa
     Cluster, ExternalSwitch, Fabric, Fan, Fantray, Interface, Linecard, Link,
     Node, PhysicalModel, Pod, Powersupply, Process, Supervisorcard,
     Systemcontroller, WorkingData,
