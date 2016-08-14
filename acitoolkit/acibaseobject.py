a5e8d5ddddc09aedac7843f457be8c7c0bdb238f
Aidan Rowe <aidan@uq.edu.au>
2016-08-15 08:24:49 +1000

    remove unneccessary if statement

--- acitoolkit/acibaseobject.py
+++ acitoolkit/acibaseobject.py
@@ -365,11 +365,10 @@ class BaseACIObject(AciSearch):
             return None
         if type(parent_class) is list:
             for parent_class_name in parent_class:
-                if not parent_class_name._get_name_from_dn(dn) is None:
-                    parent_name = parent_class_name._get_name_from_dn(dn)
-                    if parent_name is not None:
-                        parent_class = parent_class_name
-                        break
+                parent_name = parent_class_name._get_name_from_dn(dn)
+                if parent_name is not None:
+                    parent_class = parent_class_name
+                    break
         else:
             parent_name = parent_class._get_name_from_dn(dn)
         parent_dn = cls._get_parent_dn(dn)
